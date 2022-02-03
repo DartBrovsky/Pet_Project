c56d81f19d2a5b2d677b6ed10535d523cebcad81
DartBrovsky <98811143+DartBrovsky@users.noreply.github.com>
2022-02-03 22:49:18 +0200

    Fex code of test Case compare with changed page objects

--- AQA_Test/test_change_password.py
+++ AQA_Test/test_change_password.py
@@ -1,49 +1,39 @@
-from main_page import go_to_log_in
-from log_in_page import log_in
-from main_page import go_to_profile_page
-from main_page import log_out
-from profile_page import click_on_changing_password
-from profile_page import change_password
-from log_in_page import log_in_with_changed_password
+from main_page import main_page
+from log_in_page import log_in_page
+from profile_page import profile_page
 
 
 def test_change_password(browser):
+    open_main_page = main_page(browser)
+    open_main_page.get_start_page()
+    open_main_page.go_to_log_in_page()
 
-    log_in_page = go_to_log_in(browser)
-    log_in_to_site = log_in(browser)
-    go_to_profile = go_to_profile_page(browser)
-    click_on_changing_of_password = click_on_changing_password(browser)
-    change_my_password = change_password(browser)
+    log_in_to_site = log_in_page(browser)
+    local_login = "look070907@gmail.com"
+    password = "Dart_brovsky"
+    log_in_to_site.log_in(local_login, password)
 
-    log_in_page.get_start_page()
-    log_in_page.go_to_log_in_page()
-    log_in_to_site.log_in()
+    go_to_profile = main_page(browser)
     go_to_profile.go_to_profile_page()
-    click_on_changing_of_password.click_on_changing_password()
-    change_my_password.change_password()
 
+    changing_password = profile_page(browser)
+    changing_password.click_on_changing_password()
+    new_pass =  "dartdart"
+    changing_password.change_password(password, new_pass)
     assert "Password was changed" in browser.page_source
 
     # control that the password was changed
+    log_out = main_page(browser)
+    log_out.log_out()
 
-    loging_out = log_out(browser)
-
-    loging_out.log_out()
-    log_in_page.go_to_log_in_page()
-
-    log_in_with_new_password = log_in_with_changed_password(browser)
-    log_in_with_new_password.log_in_with_changed_password()
-
+    open_main_page.go_to_log_in_page()
+    log_in_to_site.log_in(local_login, new_pass)
     assert "look070907@gmail.com" in browser.page_source
 
     # return old password
-
     go_to_profile.go_to_profile_page()
-    click_on_changing_of_password.click_on_changing_password()
-
-    return_old_password = change_password(browser)
-    return_old_password.return_old_password()
-
+    changing_password.click_on_changing_password()
+    changing_password.change_password(new_pass, password)
     assert "Password was changed" in browser.page_source
 
 
