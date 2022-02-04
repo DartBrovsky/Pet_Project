acbc5bf4ea1df4ec8a7c6dbaf22b6954c5e33a36
DartBrovsky <98811143+DartBrovsky@users.noreply.github.com>
2022-02-04 11:52:59 +0200

    Create 2 page objects and 3 test cases

---
+++ AQA_Test/books_list_page.py
@@ -0,0 +1,27 @@
+from base_app import base_page
+from selenium.webdriver.common.by import By
+
+class books_list_page(base_page):
+
+    locator_log_in = (By.CSS_SELECTOR, "a.ico-login")
+    locator_register = (By.CSS_SELECTOR, "a.ico-register")
+    locator_shopping_cart = (By.CSS_SELECTOR, "a.ico-cart")
+    locator_wishlist = (By.CSS_SELECTOR, "div.header-links>ul>li>a[href='/wishlist']")
+    locator_search_field = (By.CSS_SELECTOR, "input.search-box-text")
+    locator_search_button = (By.CSS_SELECTOR, "input[type=submit]")
+    locator_log_out = (By.CSS_SELECTOR, "a[href='/logout']")
+    locator_profile_button = (By.CSS_SELECTOR, "div.header-links>ul>li>a.account")
+    locator_books_ref_button = (By.CSS_SELECTOR, "ul.top-menu>li:nth-child(1)>a")
+    locator_computers_ref_button = (By.CSS_SELECTOR, "ul.top-menu>li:nth-child(2)>a")
+    locator_electronics_ref_button = (By.CSS_SELECTOR, "ul.top-menu>li:nth-child(3)>a")
+    locator_apparelshoes_ref_button = (By.CSS_SELECTOR, "ul.top-menu>li:nth-child(4)>a")
+    locator_digitaldown_ref_button = (By.CSS_SELECTOR, "ul.top-menu>li:nth-child(5)>a")
+    locator_jewelery_ref_button = (By.CSS_SELECTOR, "ul.top-menu>li:nth-child(6)>a")
+    locator_giftcard_ref_button = (By.CSS_SELECTOR, "ul.top-menu>li:nth-child(7)>a")
+    locator_book_1 = (By.CSS_SELECTOR, "div.details>h2>a[href='/computing-and-internet']")
+    locator_book_2 = (By.CSS_SELECTOR, "div.details>h2>a[href='/copy-of-computing-and-internet']")
+    locator_book_3 = (By.CSS_SELECTOR, "div.details>h2>a[href='/fiction']")
+    locator_book_4 = (By.CSS_SELECTOR, "div.details>h2>a[href='/fiction-ex']")
+    locator_book_5 = (By.CSS_SELECTOR, "div.details>h2>a[href='/health']")
+    locator_book_6 = (By.CSS_SELECTOR, "div.details>h2>a[href='/science']")
+    
---
+++ AQA_Test/gift_cards_list_page.py
@@ -0,0 +1,25 @@
+from base_app import base_page
+from selenium.webdriver.common.by import By
+
+class gift_card_list_page(base_page):
+
+    locator_log_in = (By.CSS_SELECTOR, "a.ico-login")
+    locator_register = (By.CSS_SELECTOR, "a.ico-register")
+    locator_shopping_cart = (By.CSS_SELECTOR, "a.ico-cart")
+    locator_wishlist = (By.CSS_SELECTOR, "div.header-links>ul>li>a[href='/wishlist']")
+    locator_search_field = (By.CSS_SELECTOR, "input.search-box-text")
+    locator_search_button = (By.CSS_SELECTOR, "input[type=submit]")
+    locator_log_out = (By.CSS_SELECTOR, "a[href='/logout']")
+    locator_profile_button = (By.CSS_SELECTOR, "div.header-links>ul>li>a.account")
+    locator_books_ref_button = (By.CSS_SELECTOR, "ul.top-menu>li:nth-child(1)>a")
+    locator_computers_ref_button = (By.CSS_SELECTOR, "ul.top-menu>li:nth-child(2)>a")
+    locator_electronics_ref_button = (By.CSS_SELECTOR, "ul.top-menu>li:nth-child(3)>a")
+    locator_apparelshoes_ref_button = (By.CSS_SELECTOR, "ul.top-menu>li:nth-child(4)>a")
+    locator_digitaldown_ref_button = (By.CSS_SELECTOR, "ul.top-menu>li:nth-child(5)>a")
+    locator_jewelery_ref_button = (By.CSS_SELECTOR, "ul.top-menu>li:nth-child(6)>a")
+    locator_giftcard_ref_button = (By.CSS_SELECTOR, "ul.top-menu>li:nth-child(7)>a")
+    locator_gift_card_1 = (By.CSS_SELECTOR, "div.details>h2>a[href='/5-virtual-gift-card']")
+    locator_gift_card_2 = (By.CSS_SELECTOR, "div.details>h2>a[href='/25-virtual-gift-card']")
+    locator_gift_card_3 = (By.CSS_SELECTOR, "div.details>h2>a[href='/50-physical-gift-card']")
+    locator_gift_card_4 = (By.CSS_SELECTOR, "div.details>h2>a[href='/100-physical-gift-card']")
+
