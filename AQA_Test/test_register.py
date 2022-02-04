bf16021de580e4e6271aff77976723f9cf7ba60e
DartBrovsky <98811143+DartBrovsky@users.noreply.github.com>
2022-02-04 09:50:26 +0200

    Fix code compare with page objects code

--- AQA_Test/test_register.py
+++ AQA_Test/test_register.py
@@ -1,15 +1,14 @@
-from main_page import go_to_register
-from register_page import register
+from main_page import main_page
+from register_page import register_page
 
 
 def test_register(browser):
-    register_page = go_to_register(browser)
-    do_register = register(browser)
-    register_page.get_start_page()
-    register_page.go_to_register_page()
-    do_register.register()
+    open_main_page = main_page(browser)
+    open_main_page.get_start_page()
+    open_main_page.go_to_register_page()
 
-    browser.implicitly_wait(5)
+    do_register = register_page(browser)
+    do_register.register()
 
     assert "The specified email already exists" in browser.page_source
 
